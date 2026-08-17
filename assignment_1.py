c55209b7609249260b44472b4ffc65a2ed57de5a
0xtin <caugust19.ac@gmail.com>
2026-08-17 21:34:54 +0100

    Refactor code structure for improved readability and maintainability

---
+++ assignment_1.py
@@ -0,0 +1,29 @@
+#Exercise One
+instructor_name = "Oluwaseyi"
+num_of_students = 30
+course_name = "Python"
+print(f"The instructor is {instructor_name}, there are {num_of_students} students in the {course_name} class!")
+
+# Exercise two
+students_morning = 15
+students_evening = 25
+print(f"Before Swap: Morning Batch = {students_morning}, Evening Batch = {students_evening}")
+
+students_morning, students_evening = students_evening , students_morning
+print(f"After Swap: Morning Batch = {students_morning}, Evening Batch = {students_evening}")
+
+# Exercise three
+Python, Java, AI = 25, 18, 12
+print(f"Python= {Python}, Java= {Java}, AI= {AI}")
+
+# Exercise four
+whole_num = 21
+float_num = 4.9
+string_statement = "Python Programming"
+print(f"""
+{whole_num} is of type {(type(whole_num))}
+{float_num} is of type {type(float_num)}
+{string_statement} is of type {type(string_statement)}
+""")
+
+# Exercise four

b3647ae65e0898442a82716a3f759a376109ba3f
0xtin <caugust19.ac@gmail.com>
2026-08-18 21:39:54 +0100

    Refactor two_numbers function to use enumerate for improved readability

--- two_numbers.py
+++ two_numbers.py
@@ -1,10 +1,12 @@
 def two_numbers(nums_list: list, target: int):
     found = {}
 
-    for i in range(len(nums_list)):
-        complement = target - nums_list[i]
+    for i, num in enumerate(nums_list):
+        complement = target - num
         if complement in found:
             return [found[complement], i]
-        found[nums_list[i]] = i
+        found[num] = i
 
 print(two_numbers([2,7,11,15], 9))
+print(two_numbers([3,2,4], 6))
+print(two_numbers([3,3], 6))
